89fafc94d877b72832ff1f7684e1c27c3ff499bc
edsonbrunoa <bruno.alves.ba442@gmail.com>
2025-07-18 18:12:32 -0300

    Adicionado classe 'Political'

---
+++ Models/Political.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace falastroes_no_comando.Models
+{
+    public class Political
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public int Percent { get; set; }
+        public string Status_2 { get; set; }
+    }
+}
