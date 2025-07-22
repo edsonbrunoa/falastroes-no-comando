3224358df9f68a7d67ecab5d16d36ffcaca6c831
edsonbrunoa <bruno.alves.ba442@gmail.com>
2025-07-22 10:21:58 -0300

    Adicionado class 'Political_has_Proposal'

---
+++ Models/Political_has_Proposal.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace falastroes_no_comando.Models
+{
+    public class Political_has_Proposal
+    {
+        public int IdPolitical_has_Proposal { get; set; }
+        public int IdPolitical { get; set; }
+        public int IdProposal { get; set; }
+    }
+}
